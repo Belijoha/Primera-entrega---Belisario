9e998906df1d2387531a0c627d742af7b3b20601
Belijoha <johannabeli.3@gmail.com>
2022-02-14 01:31:21 -0300

    tercer commmit

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coins : MonoBehaviour
+public class Falling : MonoBehaviour
 {
-
+    public float fallDelay = 3.0f;
     private GameObject player;
+    
 
-    // Start is called before the first frame update
+
     void Start()
     {
         player = GameObject.FindWithTag("Player");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(0.0f, 90.0f*Time.deltaTime, 0.0f, Space.Self);
-    }
-    private void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision other)
     {
         if (other.gameObject == player)
         {
-            Destroy(gameObject);
+            StartCoroutine(FallAfterDelay());
+            
         }
     }
+
     private void OnCollisionExit(Collision other)
     {
         if (other.gameObject == player)
         {
-            Destroy(gameObject);
+            
         }
+    }
+
+    IEnumerator FallAfterDelay()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        GetComponent<Rigidbody>().isKinematic = false;
+        
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,34 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coins : MonoBehaviour
+public class Tile : MonoBehaviour
 {
-
+    
+    float timeStood = 0f;
     private GameObject player;
 
-    // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+      
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        transform.Rotate(0.0f, 90.0f*Time.deltaTime, 0.0f, Space.Self);
+
     }
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject == player)
         {
-            Destroy(gameObject);
+            timeStood += Time.deltaTime;
+            if (timeStood > 3) 
+                GetComponent<Rigidbody>().useGravity = true;
         }
     }
     private void OnCollisionExit(Collision other)
     {
         if (other.gameObject == player)
         {
-            Destroy(gameObject);
+            GetComponent<Rigidbody>().useGravity = false;
         }
     }
 }
